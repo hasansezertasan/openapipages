8ad08458a620b9f593fc67869921a8f3775790e7
Hasan Sezer Taşan <13135006+hasansezertasan@users.noreply.github.com>
2024-07-01 10:55:02 +0300

    Rename `lint` environment as `types`, add `codespell` for spell checking, update mypy and ruff configuration, update `test` environment, turn tests to async tests, update workflows accordingly, add `py.typed` to let code editors know the package is typed (#22)
    * Rename `lint` environment as `types`, add `codespell` for spell checking, update mypy and ruff configuration, update `test` environment, turn tests to async tests, update workflows accordingly, add `py.typed` to let code editors know the package is typed
    
    * Bump ruff to `0.4.5` from `0.4.3`
    
    * Add Dict from typing_extension
    
    * Ignore PLR2004 and RET504
    
    * Fix release drafter branch name
    
    * Add Release Drafter Template

--- tests/test_rapidoc.py
+++ tests/test_rapidoc.py
@@ -1,12 +1,13 @@
-from fastapi.testclient import TestClient
+import pytest
+from httpx import AsyncClient
 
 from .main import app
 
-client = TestClient(app)
 
-
-def test_rapidoc() -> None:
-    response = client.get("/rapidoc")
-    assert response.status_code == 200, response.text
-    assert response.headers["content-type"] == "text/html; charset=utf-8"
-    assert "rapidoc/dist/rapidoc-min.js" in response.text
+@pytest.mark.asyncio()
+async def test_rapidoc() -> None:
+    async with AsyncClient(app=app, base_url="http://testserver/") as client:
+        response = await client.get("/rapidoc")
+        assert response.status_code == 200, response.text
+        assert response.headers["content-type"] == "text/html; charset=utf-8"
+        assert "rapidoc/dist/rapidoc-min.js" in response.text
--- tests/test_scalar.py
+++ tests/test_scalar.py
@@ -1,18 +1,21 @@
-from fastapi.testclient import TestClient
+import pytest
+from httpx import AsyncClient
 
 from .main import app
 
-client = TestClient(app)
+
+@pytest.mark.asyncio()
+async def test_scalar_plain() -> None:
+    async with AsyncClient(app=app, base_url="http://testserver/") as client:
+        response = await client.get("/scalar-plain")
+        assert response.status_code == 200, response.text
+        assert response.headers["content-type"] == "text/html; charset=utf-8"
+        assert "@scalar/api-reference" in response.text
 
 
-def test_scalar_plain() -> None:
-    response = client.get("/scalar-plain")
-    assert response.status_code == 200, response.text
-    assert response.headers["content-type"] == "text/html; charset=utf-8"
-    assert "@scalar/api-reference" in response.text
-
-
-def test_scalar_custom() -> None:
-    response = client.get("/scalar-custom")
-    assert response.status_code == 200, response.text
-    assert "https://cdn.jsdelivr.net/npm/@scalar/api-reference" in response.text
+@pytest.mark.asyncio()
+async def test_scalar_custom() -> None:
+    async with AsyncClient(app=app, base_url="http://testserver/") as client:
+        response = await client.get("/scalar-custom")
+        assert response.status_code == 200, response.text
+        assert "https://cdn.jsdelivr.net/npm/@scalar/api-reference" in response.text
