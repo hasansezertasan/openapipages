6e616f86ba8e33ff91eba8e659d978039bb8bb82
hasansezertasan <hasansezertasan@gmail.com>
2025-07-31 21:16:39 +0300

    refactor(tests): update AsyncClient usage to utilize ASGITransport for all test files

--- tests/test_redoc.py
+++ tests/test_redoc.py
@@ -1,6 +1,6 @@
 import pytest
 from fastapi import status
-from httpx import AsyncClient
+from httpx import ASGITransport, AsyncClient
 
 from tests.main import app
 
@@ -8,7 +8,7 @@ from tests.main import app
 @pytest.mark.asyncio
 async def test_redoc_plain() -> None:
     # Copy of  https://github.com/tiangolo/fastapi/blob/be876902554a0bd886167de144f0d593ed2e6ad7/tests/test_application.py#L42-L46
-    async with AsyncClient(app=app, base_url="http://testserver/") as client:
+    async with AsyncClient(transport=ASGITransport(app), base_url="http://testserver/") as client:
         response = await client.get("/redoc-plain")
         assert response.status_code == status.HTTP_200_OK, response.text
         assert response.headers["content-type"] == "text/html; charset=utf-8"
@@ -19,7 +19,7 @@ async def test_redoc_plain() -> None:
 @pytest.mark.asyncio
 async def test_redoc_custom() -> None:
     # Copy of  https://github.com/tiangolo/fastapi/blob/be876902554a0bd886167de144f0d593ed2e6ad7/tests/test_tutorial/test_custom_docs_ui/test_tutorial001.py#L35-L38
-    async with AsyncClient(app=app, base_url="http://testserver/") as client:
+    async with AsyncClient(transport=ASGITransport(app), base_url="http://testserver/") as client:
         response = await client.get("/redoc-custom")
         assert response.status_code == status.HTTP_200_OK, response.text
         assert (
